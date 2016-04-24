fd9b1d890c4dbfbc534f9954e3cd3f7831eb8b77
Conor Sayres <csayres@uw.edu>
2016-04-24 15:44:46 -0300

    remove commented code.

--- bin/emulateLCOTCC.py
+++ bin/emulateLCOTCC.py
@@ -24,48 +24,9 @@ from lcoTCC.actor import TCCLCOActor
 from lcoTCC.dev import TCSDevice, ScaleDevice, FakeScaleCtrl, FakeTCS
 
 UserPort = 25000
-UDPPort = 25010
 
 ScaleDevicePort = 26000
 TCSDevicePort = 27000
-
-# root = Tkinter.Tk()
-# root.geometry("900x400+200+50")
-# twisted.internet.tksupport.install(root)
-# root.wm_withdraw()
-
-
-# RO.Wdg.stdBindings(root)
-
-# class MockTCCLCOActor(TCCLCOActor):
-#     def __init__(self):
-#         self.name = "mockTCCLCO"
-#         TCCLCOActor.__init__(self,
-#             name = self.name,
-#             userPort = UserPort,
-#             udpPort = UDPPort,
-#             tcsDev = TCSDevice("mockTCS", "localhost", TCSDevicePort),
-#             scaleDev = ScaleDevice("mockScale", "localhost", ScaleDevicePort),
-#         )
-#         self.server.addStateCallback(self._serverStateCallback)
-#         self.actorClientWdg = None
-
-#     def _serverStateCallback(self, sock):
-#         """Actor server state callback; use to construct a client window
-#         """
-#         if sock.isReady:
-#             tccTopLevel = RO.Wdg.Toplevel(
-#                 master = root,
-#                 title = self.name,
-#                 geometry = "1000x500+300+200",
-#             )
-#             self.actorClientWdg = ActorClientWdg(
-#                 master = tccTopLevel,
-#                 name = "tcc", # name of keyword dictionary
-#                 host = "localhost",
-#                 port = UserPort,
-#             )
-#             self.actorClientWdg.pack(fill="both", expand=True)
 
 print "Start fake LCO controllers"
 fakeScaleController  = FakeScaleCtrl("fakeScale",  ScaleDevicePort)
@@ -76,7 +37,6 @@ def startTCCLCO(*args):
         tccActor = TCCLCOActor(
             name = "tccLCOActor",
             userPort = UserPort,
-            udpPort = UDPPort,
             tcsDev = TCSDevice("tcsDev", "localhost", TCSDevicePort),
             scaleDev = ScaleDevice("mockScale", "localhost", ScaleDevicePort),
             )
