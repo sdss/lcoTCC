e2d866a3ff11262909bb1b8f5ac424accff9fbd8
Conor Sayres <csayres@uw.edu>
2017-03-17 17:03:23 -0300

    add lamp command

---
+++ python/tcc/cmd/lamp.py
@@ -0,0 +1,26 @@
+from __future__ import division, absolute_import
+
+from twistedActor import LinkCommands
+
+__all__ = ["lamp"]
+
+def lamp(tccActor, userCmd):
+    """Implement direct ff lamp command
+
+    @param[in,out] tccActor  tcc actor
+
+    @param[in,out] userCmd  a twistedActor BaseCommand with parseCmd attribute
+    """
+    params = userCmd.parsedCmd.paramDict
+    action = params["action"].valueList[0].keyword
+    if action == "off":
+        tccActor.ffDev.powerOff(userCmd)
+    elif action == "on":
+        tccActor.ffDev.powerOn(userCmd)
+    else:
+        tccActor.ffDev.getStatus(userCmd)
+
+
+
+
+
